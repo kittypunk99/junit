84a1301a234cb5b1e74e357a45af8aed364fe259
junioradmin <Linus@freistetter.com>
2024-09-04 10:21:48 +0200

    waow fertig

--- src/junit/Speise.java
+++ src/junit/Speise.java
@@ -7,13 +7,13 @@ import java.util.List;
 public class Speise {
 
     public static final List<Speise> allMeals = Arrays.asList(new Speise("Fritattensuppe;Suppe;nein;4.2;360;ACFGLM"), new Speise("Gulaschsuppe;Suppe;nein;4.9;312;ALO"), new Speise("Knoblauchcremesuppe;Suppe;nein;4.7;500;AGLM"), new Speise("Beef Tartare;Vorspeise;nein;13.9;425;ACM"), new Speise("Weinbergschnecken;Vorspeise;nein;11.9;330;AGR"), new Speise("Rindercarpaccio mit Fladenbrot;Vorspeise;nein;9.9;435;ACGNP"), new Speise("Schweinsbraten mit Krautsalat und Serviettenknoedel;Hauptspeise;nein;11.7;781;ACG"), new Speise("Wiener Schnitzel vom Schwein mit Salat;Hauptspeise;nein;9.6;518;ACG"), new Speise("Pute natur mit Reis;Hauptspeise;nein;9.2;223;AG"), new Speise("Zander vom Grill;Hauptspeise;nein;14.9;353;ADG"), new Speise("Zwiebelrostbraten;Hauptspeise;nein;15.5;336;ALM"), new Speise("Steirischer Backhendlsalat;Hauptspeise;nein;9.7;700;ACGM"), new Speise("Kaesespaetzle mit gemischtem Salat;Hauptspeise;ja;8.5;1025;CGM"), new Speise("Chili sin carne mit geraeuchertem Tofu;Hauptspeise;ja;7.2;250;ACFGN"), new Speise("Lauchpancakes mit Kaesesauce;Hauptspeise;ja;7.9;948;ACGL"), new Speise("Geroestete Knoedel mit Ei & Salat;Hauptspeise;ja;6.9;280;ACG"), new Speise("Schokoladepalatschinken mit Schlagobers & Nuessen;Nachspeise;ja;4.9;310;ACEG"), new Speise("Gebackene Apfelspalten;Nachspeise;ja;5.9;350;ACFHN"), new Speise("Topfenknoedel mit Zwetschkenroester;Nachspeise;ja;4.5;222;ACG"), new Speise("Schokomousse-Trilogie;Nachspeise;ja;8.5;457;CEFG"));
+    private final static String allergeneListe = "ACDEFGHLMNOPR";
     private final String name;
     private final String kategorie;
     private final boolean vegetarisch;
     private final double preis;
     private final int kalorien;
     private final String allergene;
-    private final static String allergeneListe = "ACDEFGHLMNOPR";
 
     public Speise(String allInfo) {
         String[] details = allInfo.split(";");
@@ -21,8 +21,8 @@ public class Speise {
         if (details.length != 6) {
             throw new IllegalArgumentException("Falsche Anzahl an Attributen");
         }
-        for (int i = 0; i < details.length; i++) {
-            if (details[i].isEmpty()) {
+        for (String detail : details) {
+            if (detail.isEmpty()) {
                 throw new IllegalArgumentException("Ein oder mehrere Attribute sind leer");
             }
         }
--- src/junit/TestSpeise.java
+++ src/junit/TestSpeise.java
@@ -2,8 +2,6 @@ package junit;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.stream.Collectors;
-
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -54,20 +52,18 @@ public class TestSpeise {
     @Test
     public void testGetters() {
         Speise speise = new Speise("essen;suppe;nein;3.0;200;ACFGLM");
-        assert(speise.getName().equals("essen"));
-        assert(speise.getKategorie().equals("suppe"));
-        assert(!speise.isVegetarisch());
-        assert(speise.getPreis() == 3.0);
-        assert(speise.getKalorien() == 200);
-        assert(speise.getAllergene().equals("ACFGLM"));
-        assert(speise.toString().equals("essen"));
+        assert (speise.getName().equals("essen"));
+        assert (speise.getKategorie().equals("suppe"));
+        assert (!speise.isVegetarisch());
+        assert (speise.getPreis() == 3.0);
+        assert (speise.getKalorien() == 200);
+        assert (speise.getAllergene().equals("ACFGLM"));
+        assert (speise.toString().equals("essen"));
     }
 
     @Test
     public void testSpeiseKarte() {
-        assertDoesNotThrow(() -> {
-            SpeiseKarte.main(null);
-        });
+        assertDoesNotThrow(() -> SpeiseKarte.main(null));
     }
 
 }
